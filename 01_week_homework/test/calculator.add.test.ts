ee00269f6756cee7a24ec2a7db293861e564e096
Szabo Denes <szabo.denes@internode.hu>
2024-05-09 11:53:01 +0200

    Add 1st week homework - addition test

---
+++ 01_week_homework/test/calculator.add.test.ts
@@ -0,0 +1,58 @@
+import { Calculator } from "../src/calculator";
+
+describe("Calculator tests", () => {
+    let calculator: Calculator;
+    const testCases = [
+        {
+            a: 5,
+            b: 3,
+            expectedResult: 8,
+        },
+        {
+            a: 5,
+            b: -3,
+            expectedResult: 2,
+        },
+        {
+            a: 5,
+            b: -7,
+            expectedResult: -2,
+        },
+        {
+            a: -5,
+            b: -2,
+            expectedResult: -7,
+        },
+        {
+            a: 5,
+            b: 0,
+            expectedResult: 5,
+        },
+        {
+            a: 0,
+            b: 5,
+            expectedResult: 5,
+        },
+        {
+            a: -2,
+            b: 5,
+            expectedResult: 3,
+        },
+    ];
+
+    beforeEach(() => {
+        console.log('new calculator');
+        calculator = new Calculator();
+    })
+
+    testCases.forEach((testCase) => {
+        test(`'Given I have 2 numbers, When I add them together, Then the sum of the two numbers is returned (${testCase.a} + ${testCase.b} = ${testCase.expectedResult})'`, () => {
+            // Act
+            const result = calculator.Add(testCase.a, testCase.b);
+
+            // Assert
+            expect(result).toBe(testCase.expectedResult);
+        });
+    })
+
+})
