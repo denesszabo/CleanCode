3e1646e9c1072439a473dcb9ed3c0a41d10b00a1
Szabo Denes <szabo.denes@internode.hu>
2024-05-09 12:20:25 +0200

    Add multiplication function tests

---
+++ 01_week_homework/test/calculator.multiplication.test.ts
@@ -0,0 +1,79 @@
+import { Calculator } from "../src/calculator";
+
+describe("Calculator multiplication function tests", () => {
+    let calculator: Calculator;
+
+    const testCases = [
+        {
+            a: 5,
+            b: 3,
+            expectedResult: 15,
+        },
+        {
+            a: 3,
+            b: 5,
+            expectedResult: 15,
+        },
+        {
+            a: 5,
+            b: -3,
+            expectedResult: -15,
+        },
+        {
+            a: 5,
+            b: -7,
+            expectedResult: -35,
+        },
+        {
+            a: -5,
+            b: -2,
+            expectedResult: 10,
+        },
+        {
+            a: 5,
+            b: 0,
+            expectedResult: 0,
+        },
+        {
+            a: 0,
+            b: 5,
+            expectedResult: 0,
+        },
+        {
+            a: -2,
+            b: 5,
+            expectedResult: -10,
+        },
+        // Floating point overflow!
+        {
+            a: 5.2,
+            b: 3.3,
+            expectedResult: 5.2 * 3.3,
+        },
+        {
+            a: 5.2,
+            b: -3.3,
+            expectedResult: 5.2 * -3.3,
+        },
+        {
+            a: -3.3,
+            b: 5.2,
+            expectedResult: -3.3 * 5.2,
+        },
+    ];
+
+    beforeEach(() => {
+        calculator = new Calculator();
+    })
+
+    testCases.forEach((testCase) => {
+        test(`'Given I have 2 numbers, When I multiply them, Then the two numbers multiplication result is returned (${testCase.a} * ${testCase.b} = ${testCase.expectedResult})'`, () => {
+            // Act
+            const result = calculator.Multiply(testCase.a, testCase.b);
+
+            // Assert
+            expect(result).toBe(testCase.expectedResult);
+        });
+    })
+
+})
