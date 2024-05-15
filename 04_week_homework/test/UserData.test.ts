a470d7ff91bfd653deeb87a1a627dc1194de65cc
Szabo Denes <szabo.denes@internode.hu>
2024-05-15 15:00:28 +0200

    Week 04: Add UserWorkdayReports test

---
+++ 04_week_homework/test/UserData.test.ts
@@ -0,0 +1,20 @@
+import {UserWorkdayReports} from "../src/UserWorkdayReports";
+
+describe('Process user data tests', () => {
+    let sut = new UserWorkdayReports();
+
+    beforeEach(() => {
+        sut = new UserWorkdayReports();
+    })
+    describe('Happy path', () => {
+        it('should return with not success', () => {
+
+        });
+    })
+
+    describe('Sad path', () => {
+        it('should return with no action taken', () => {
+
+        });
+    })
+})
