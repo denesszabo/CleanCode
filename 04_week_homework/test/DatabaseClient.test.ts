0d3891c8b3394d2e161c15220441dae8cb8fdb7d
Szabo Denes <szabo.denes@internode.hu>
2024-05-15 15:00:14 +0200

    Week 04: Add databaseClient test

---
+++ 04_week_homework/test/DatabaseClient.test.ts
@@ -0,0 +1,51 @@
+import { OutOfRangeError } from "../src/error/OutOfRangeError";
+import {DatabaseClient} from "../src/DatabaseClient";
+describe('DatabaseClient tests', () => {
+    let sut: DatabaseClient;
+
+    beforeEach(() => {
+        sut = new DatabaseClient();
+    })
+
+
+    describe('Happy path', () => {
+
+        it.each([
+            { year: 2022, month: 1},
+            { year: 2024, month: 11},
+        ])('should return with success for $month', ({year, month}) => {
+            // Arrange
+            const result = sut.getWorkdays(year, month);
+            expect(result).toBeTruthy();
+            expect(result.length).toBeLessThanOrEqual(DatabaseClient.MAX_WORKDAYS_IN_MONTH);
+        });
+    })
+    describe('Sad path', () => {
+
+        it.each([
+            { year: 2019, month: 0},
+            { year: 2010, month: 11},
+            { year: 2055, month: 5},
+        ])('should return with exception for $year - $month', ({year, month}) => {
+            // Arrange
+            const expectedError = new OutOfRangeError('Invalid year given');
+            // const result = sut.getWorkdays(year, month);
+
+            // Assert
+            expect(() =>sut.getWorkdays(year, month)).toThrow(expectedError);
+        });
+
+        it.each([
+            { year: 2022, month: -1},
+            { year: 2023, month: 12},
+            { year: 2024, month: 13},
+        ])('should return with exception for $year - $month', ({year, month}) => {
+            // Arrange
+            const expectedError = new OutOfRangeError('Invalid month given');
+            // const result = sut.getWorkdays(year, month);
+
+            // Assert
+            expect(() => sut.getWorkdays(year, month)).toThrow(OutOfRangeError);
+        });
+    })
+})
